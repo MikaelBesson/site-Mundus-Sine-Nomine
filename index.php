1236e37b3265d58b1ff35ebe7da105b179e569d6
BessonMikael <besson.mikael04@gmail.com>
2021-03-29 15:55:19 +0200

    debut du projet mundus

---
+++ index.php
@@ -0,0 +1,44 @@
+<?php
+?>
+
+<!doctype html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="css/style.css">
+    <title>Acceuil</title>
+</head>
+<body>
+    <div id="slide1">
+        <div class="slide_inside">
+            <figure>
+                <img src="images/titre.png" width="100%" alt="">
+            </figure>
+            
+        </div>
+
+    </div>
+
+    <div id="slide2">
+        <div class="slide_inside">
+            
+        </div>
+
+    </div>
+
+    <div id="slide3">
+        <div class="slide_inside">
+            
+        </div>
+
+    </div>
+    <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js">
+</body>
+</html>
+
+
+
+
