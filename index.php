a9185e673a010799b687d084f42098873338dfa8
BessonMikael <besson.mikael04@gmail.com>
2021-05-10 16:32:45 +0200

    presentation et ajout du connect sign it

--- index.php
+++ index.php
@@ -8,24 +8,36 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <script src="https://kit.fontawesome.com/940412168c.js" crossorigin="anonymous"></script>
     <link rel="stylesheet" href="css/style.css">
     <title>Acceuil</title>
 </head>
 <body>
+
     <div id="slide1">
         <div class="slide_inside">
+            <nav>
+                <a id="connect" href=""><i class="fas fa-user-plus"></i></a>
+            </nav>
             <figure>
                 <img src="images/titre.png" width="100%" alt="">
             </figure>
         </div>
         <h1 class="ml2">La communauté francophone des jeux de survie</h1>
+        <div class="presentation">
+            Ce site a pour vocation de réunir une communauté française autour des jeux de survies que nous aimons tant.<br>
+            Il va faciliter les échanges / partages / découvertes ainsi que les parties entre ses membres,<br>
+            sur des serveurs de qualité mis à leur disposition.<br>
+
+            Cet espace étant publique, vous serez amenés à y côtoyer des gens de tous horizons <br>dont les goûts
+            / idées / avis seront potentiellement différents des vôtres. <br>Nous vous demandons donc de faire preuve
+            de tolérance, de respect et de bienséance lors de vos interventions.
+        </div>
     </div>
 
     <div id="slide2">
         <div class="slide_inside">
-            <p>
 
-            </p>
             
         </div>
 
